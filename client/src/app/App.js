caa063726771f5d23b810aedc4ae7160bee78d41
gdsduarte <gds_duarte@hotmail.com>
2023-04-05 15:15:12 +0100

    Implementing XHR

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -2,13 +2,23 @@ import Home from "../components/HomePage";
 import Login from "../components/LoginPage";
 import { useAuth } from "../services/AuthProvider";
 import React, { useEffect } from "react";
-import { fetchServerStatus } from "../services/http-common";
+import { fetchData } from "../services/fetchData"; // Import the fetchData function
 
 function App() {
   const { user, role } = useAuth();
-  
+
   useEffect(() => {
-    fetchServerStatus();
+    const onSuccess = (response) => {
+      console.log("Raw response data:", response);
+      const jsonData = JSON.parse(response);
+      console.log("Server status:", jsonData);
+    };
+
+    const onError = (error) => {
+      console.error("Error fetching server status:", error);
+    };
+
+    fetchData("/api/server-status", onSuccess, onError);
   }, []);
 
   return (
