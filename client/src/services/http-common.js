941dc41012228e174bc67506c191dfbac56ee266
gdsduarte <gds_duarte@hotmail.com>
2023-04-05 12:09:42 +0100

    Update Axios IP

--- client/src/services/http-common.js
+++ client/src/services/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const isDevelopment = process.env.NODE_ENV === "development";
 
 const http = axios.create({
-  baseURL: isDevelopment ? "http://localhost:5000" : "http://34.142.56.192:5000",
+  baseURL: isDevelopment ? "http://localhost:5000" : "http://35.242.175.209:5000",
   headers: {
     "Content-type": "application/json",
   },
